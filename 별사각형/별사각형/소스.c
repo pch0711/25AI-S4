449c27f469a27c0347642dc6c79aabc43de3011f
20251408 <06pch1066@naver.com>
2025-09-10 14:48:23 +0900

    FNF4

---
+++ 별사각형/별사각형/소스.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+int main() {
+    int i = 0;
+    int j;
+
+    while (i < 5) 
+    {
+        j = 0;
+        while (j < 5) 
+        {
+            if (i == 0 || i == 4 || j == 0 || j == 4) 
+            {
+                printf("*");
+            }
+            else 
+            {
+                printf(" ");
+            }
+            j = j+1;
+        }
+        printf("\n");
+        i = i+1;
+    }
+
+    return 0;
+}
